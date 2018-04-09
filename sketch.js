ec98442df06a932e6ce6d0f39b7d99b886c3aaa4
EmmJea <38199095+EmmJea@users.noreply.github.com>
2018-04-09 05:26:15 -0700

    Imported prank file

--- sketch.js
+++ sketch.js
@@ -1,7 +1,38 @@
+
+var bgshade=0
+var img;
+var hedero;
+
+function preload() {
+img = loadImage('Rainbow.gif');
+}
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	background(bgshade);
+	hedero = loadSound('Hedero.wav');
+
 }
 
 function draw() {
-	rect(500, 500, 100, 100)
+
+}
+
+function keyPressed() {
+	if (bgshade === 0) {
+		bgshade = 255;
+		background(bgshade);
+		image(img, 0, 0, 1930, 1930);
+		textSize(128);
+		textAlign(CENTER);
+		textStyle(BOLD);
+		text('YOU HAVE BEEN INVADED', 960, 430);
+		text('BY THE GAYS', 960, 650);
+		masterVolume(3);
+		hedero.loop();
+
+
+	} else {
+		return false;
+	}
 }
